9d165536e8bdeceb4a902397b40fa79e534ed456
samuel <samuelgarciacompanys@gmail.com>
2020-02-25 13:58:54 +0100

    Styled project components

--- app/javascript/components/projects/Project.js
+++ app/javascript/components/projects/Project.js
@@ -6,12 +6,13 @@ class Project extends React.Component {
       <div className="projectAlone col-lg-12 col-sm-12">
         <div className="projectAlone-within d-flex flex-row flex-wrap justify-content-center">
         <h3 className="title-project"> {this.props.name}</h3>
-        <div className="d-flex flex-row flex-wrap justify-content-center">
-        <a href={this.props.url} target="_blank">
-        <img class="img-project" src={this.props.img} />
-        </a>
-        <p className="">
-        {this.props.description}</p>
+        <div className="image-description-container">
+          <div className="image-container">
+          <a href={this.props.url} target="_blank">
+          <img class="img-project" src={this.props.img} />
+          </a>
+          </div>
+          <p className="">{this.props.description}</p>
         </div>
       </div>
       </div>
