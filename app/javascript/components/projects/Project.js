e818aaaefe07bacb90b5f9f4206b7b8144ca0e4d
samuel <samuelgarciacompanys@gmail.com>
2020-02-29 20:06:28 +0100

    make it better

--- app/javascript/components/projects/Project.js
+++ app/javascript/components/projects/Project.js
@@ -5,7 +5,9 @@ class Project extends React.Component {
     return (
       <div className="projectAlone col-lg-12 col-sm-12">
         <div className="projectAlone-within d-flex flex-row flex-wrap justify-content-center">
+        <a href={this.props.livelink} target="_blank">
         <h3 className="title-project"> {this.props.name}</h3>
+        </a>
         <div className="image-description-container">
           <div className="image-container">
           <a href={this.props.url} target="_blank">
